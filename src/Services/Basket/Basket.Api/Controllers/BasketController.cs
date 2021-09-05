a39784a3c6469e2373045cb0e5866fb2d029be52
Yusuff Bello <ykbello2011@gmail.com>
2021-09-05 01:36:42 +0100

    Basket M.Service Added

---
+++ src/Services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using Basket.Api.Entities;
+using Basket.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepo;
+
+        public BasketController(IBasketRepository basketRepo)
+        {
+            _basketRepo = basketRepo;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), 200)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _basketRepo.GetBasket(userName);
+
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost(Name = "UpdateBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), 200)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket(ShoppingCart basket) => Ok(await _basketRepo.UpdateBasket(basket));
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), 200)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepo.DeleteBasket(userName);
+
+            return Ok();
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IConfiguration _config;
+
+        public CatalogContext(IConfiguration config)
+        {
+            _config = config;
+            var client = new MongoClient(_config["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(_config["DatabaseSettings:DatabaseName"]);
+
+            Products = database.GetCollection<Product>(_config["DatabaseSettings:CollectionName"]);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
